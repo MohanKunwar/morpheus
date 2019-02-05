6da2de103f075cf80d45951c9d4834a3f9297bea
aaryan <aaryal174@gmail.com>
2019-02-05 17:28:19 +0545

    add business registration form

---
+++ src/containers/components/AddBusinessForm/AddBusiness.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './AddBusiness.css';
+import { Field } from 'react-final-form';
+class AddBusiness extends Component {
+    render() {
+        return (
+            <div> 
+            <h1>Khozinfo Registration Form</h1>
+                <form
+                onlogin={onlioin}
+                initialValues={{ stooge: 'larry', employed: false }}
+                render={({ handlelogin, form, submitting, pristine, values })} => (
+                    <form onlogin={handlelogin})>
+
+            </div>
+            <div>  
+            <label>Username</label>
+            <Field
+            name="Username"
+            Component="import"
+            type="text"
+            placeholder="Username"
+            />
+            </div>
+            <div>
+            <leble>Mobile Number or Email Address</leble>
+            <form
+            name="Mobile Number or Email Address"
+            Component="import"
+            type="text"
+            placeholder="Mobile Number or Email Address"
+            />
+            </div>
+
+            <div>
+            <label>Password</label>
+            <form
+            name="Password"
+            Component="import"
+            type="text"
+            placeholder="Password"
+            />
+            </div>
+            <div className="Login"
+            <button
+            type="button"
+            disabled={Login}>
+            Login
+            </button>
+            
+
+        );
+    }
+}
+
+export default AddBusiness;
