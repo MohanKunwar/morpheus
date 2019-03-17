c597d42889c4eecfaca6fd1bc4d2d3311cf9c2fe
manozk <manozk@khozinfo.com>
2019-03-17 13:44:42 +0545

    updated requirement list request reasons and close calls

--- src/components/user/Requirements/RequirementList.js
+++ src/components/user/Requirements/RequirementList.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import Img from "react-image";
+import { withRouter } from 'react-router-dom';
 import "./requirement.css";
 import Dropdown from "../../../UI/Dropdown/Dropdown";
 import Axios from "../../../services/Axios";
 class RequirementList extends Component {
+  item
+  otherReason
   state = {
-    showCloseForm: false
+    showCloseForm: false,
+    closeReason: null,
+    otherReason: ''
   };
-  closeRequest = e => {
+  closeRequest = (e, item) => {
+    this.item = item
     e.preventDefault();
     if (!this.state.closeReasons) {
       Axios.instance
@@ -19,9 +25,33 @@ class RequirementList extends Component {
           });
         });
     } else {
-        this.setState({showCloseForm: true})
+      this.setState({ showCloseForm: true })
     }
-  };
+  }
+  closeReasonSelected = (e, reason) => {
+    this.setState({closeReason: reason})
+  }
+  submitCloseForm = (e, itemId) => {
+    let payload = {}
+      if (this.state.closeReason.id === 4) {
+        payload.lead_close_reason_other = this.otherReason
+      }
+      payload.close_reason_id = this.state.closeReason.id
+      Axios.authInstance.post(Axios.API.requirement.closeRequirementUrl(itemId), payload).then(
+        response => {
+          if (response && response.status) {
+            switch (response.status) {
+              case 204: {
+                this.props.history.push('/user/requirements/completed')
+                break
+              }
+              default: {
+                break
+              }
+            }
+          }
+        })
+  }
   render() {
     return this.props.items && this.props.items.length > 0 ? (
       this.props.items.map((item, index) => (
@@ -46,8 +76,8 @@ class RequirementList extends Component {
               {item.status === "pending"
                 ? "Approval Pending"
                 : item.status === "approved"
-                ? "Approved"
-                : "Request Completed"}
+                  ? "Approved"
+                  : "Request Completed"}
             </span>
             {item.status === "approved" && !item.closed_at ? (
               <button
@@ -57,25 +87,54 @@ class RequirementList extends Component {
                 Close Request
               </button>
             ) : null}
-            {this.state.showCloseForm ? (
-              <Dropdown
-                content={this.state.closeReasons.map((reason, index) => (
-                  <div key={index}>
-                    <input type="checkbox" value={reason.id} />
-                    {reason.reason_text}
-                  </div>
-                ))}
-              />
-            ) : null}
+            {
+              this.state.showCloseForm && this.item === item
+                ?
+                  <Dropdown
+                    content={
+                      <React.Fragment>
+                      {
+                        this.state.closeReasons.map((reason, index) => (
+                      <React.Fragment key={index}>
+                        <input type='checkbox' 
+                          onChange={e => this.closeReasonSelected(e, reason)} 
+                          checked={this.state.closeReason ? this.state.closeReason.id===reason.id ? 'checked' : false : false} />
+                        {
+                          reason.reason_text !== 'I dont need this requirement anymore'
+                          ? <span className='reason_text'>{reason.reason_text}</span>
+                          : <input type='text' 
+                              disabled={this.state.closeReason ? this.state.closeReason.id !== reason.id ? true : false : true} 
+                              placeholder='Other reason' 
+                              defaultValue={this.state.closeReason ? this.state.closeReason.id === reason.id ? this.state.otherReason : '' : ''} 
+                              onChange={e => this.setState({otherReason: e.target.value})}/>
+                        }
+                      </React.Fragment>
+                    ))}
+                      <button type='submit' 
+                        disabled={this.state.closeReason 
+                          ? this.state.closeReason.id === 4  
+                            ? this.state.otherReason
+                              ? false
+                              : true
+                            : false
+                          : true} 
+                        onClick={e => this.submitCloseForm(e, item.id)}>Close Now</button>
+                      </React.Fragment>
+                    }
+                  />
+                  
+                :
+                null
+            }
 
             <span>{item.closed_at ? <span>closed</span> : null}</span>
           </div>
         </div>
       ))
     ) : (
-      <div>no items found</div>
-    );
+        <div>no items found</div>
+      );
   }
 }
 
-export default RequirementList;
+export default withRouter(RequirementList);
