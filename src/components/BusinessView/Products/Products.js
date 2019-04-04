f31a325efa1b32237c98b680a02741762b17fb2a
parbatisunari <46644300+parbatisunari@users.noreply.github.com>
2019-04-04 17:26:56 +0545

    hotel room list and product list css

--- src/components/BusinessView/Products/Products.js
+++ src/components/BusinessView/Products/Products.js
@@ -5,6 +5,7 @@ const Products = props => {
 return (
     props.products.map((product, index) => 
         <div key={index} className='business_products_container'>
+        <h4 className="business_product_title">Product/Service</h4>
             <BusinessProductCard product={product} />
         </div>
     )
