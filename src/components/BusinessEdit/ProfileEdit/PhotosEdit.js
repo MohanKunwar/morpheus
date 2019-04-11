ff5eca93a6b0ce1e7a7df0608fc28837dd60bf5c
Ganesh <ganeshkoirala999@gmail.com>
2019-04-11 11:03:30 +0545

    added ImagesPreview Component in PhotosEdit

--- src/components/BusinessEdit/ProfileEdit/PhotosEdit.js
+++ src/components/BusinessEdit/ProfileEdit/PhotosEdit.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import Spinner from '../../../helpers/Spinner';
-
+import Images from '../../common/ImagesPreview/ImagesPreview'
 export default class PhotosEdit extends Component {
     render() {
         return (
             this.props.photos
             ?
             <div className='business_photos_edit'>
-                photos
+                <Images />
             </div>
             : <Spinner />
         )
