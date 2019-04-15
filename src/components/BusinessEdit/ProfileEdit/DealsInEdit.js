090b512c93377bc56828931f537579c47337edde
Ganesh <ganeshkoirala999@gmail.com>
2019-04-15 17:21:16 +0545

    scripted deals in category

--- src/components/BusinessEdit/ProfileEdit/DealsInEdit.js
+++ src/components/BusinessEdit/ProfileEdit/DealsInEdit.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import Spinner from '../../../helpers/Spinner';
-
+import "./DealsInEdit.css";
+var checked= [];
 export default class DealsInEdit extends Component {
+    handleChecked= (e) => {
+        if(e.target.checked ===  true){
+            checked.push(e.target.value)
+            console.log(checked);
+        }
+        else{
+            checked.splice( checked.indexOf('foo'), 1 );
+            console.log(checked)
+        }
+    }
     render() {
         console.log(this.props.dealsIn)
         return (
             this.props.dealsIn
             ?
             <div className='business_dealsin_edit'>
-                DealsIn
+          {  this.props.dealsIn.map((dealsIn) => {
+            return <div className="dealsin_list" key={dealsIn.id}>
+            <input onChange={this.handleChecked} type="checkbox" id={dealsIn.id} value={dealsIn.id}/> 
+            <label htmlFor={dealsIn.id}>{dealsIn.name}</label>
+            </div>
+            }) }
             </div>
             : <Spinner />
         )
