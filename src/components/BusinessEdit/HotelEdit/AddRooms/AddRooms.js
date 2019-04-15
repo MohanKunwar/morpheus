48a858dcae231168a2a853ff8a5006c4437a04ea
parbatikhoz <parbati@khozinfo.com>
2019-04-15 17:40:44 +0545

    room add form validation

--- src/components/BusinessEdit/HotelEdit/AddRooms/AddRooms.js
+++ src/components/BusinessEdit/HotelEdit/AddRooms/AddRooms.js
@@ -4,6 +4,7 @@ import ImagesPreview from '../../../common/ImagesPreview/ImagesPreview'
 import './AddRooms.css'
 import { Form } from 'react-final-form'
 import Inputfield from './../../../../UI/Inputfield/inputfield'
+import Error from './../../../../helpers/FormError'
 
 class AddRooms extends Component{
     state ={
@@ -17,6 +18,11 @@ class AddRooms extends Component{
             })
     }
 
+    onSubmit = values => {
+        
+      }
+    
+
     render(){
         console.log(this.state.allhotelAmenities)
         return(
@@ -26,7 +32,33 @@ class AddRooms extends Component{
             {/* <span>Add New Room</span>
             <p className='upload_room_img'><ImagesPreview /></p> */}
             </div>
-                <Form onSubmit={this.onSubmit}>
+                <Form onSubmit={this.onSubmit}
+                validate = {values => {
+                    const errors = {};
+                    if (!values.add_room_name) {
+                        errors.add_room_name = 'Room name is required'
+                      }
+                      if (!values.add_room_price) {
+                        errors.add_room_price = 'Price is required'
+                      } else if(isNaN(values.add_room_price)){
+                          errors.add_room_price ='Price must be number'
+                      }
+                      if (!values.add_room_count) {
+                        errors.add_room_count = 'Room count is required'
+                      }
+                      if (!values.add_max_capacity) {
+                        errors.add_max_capacity = 'Room maximum capacity is required'
+                      }
+                      if (!values.add_room_des) {
+                        errors.add_room_des = 'Room description is required'
+                      }
+                      if (!values.add_room_rules) {
+                        errors.add_room_rules = 'Room rules are required'
+                      }
+                      
+                      return errors
+                }}
+                >
                 {({ handleSubmit}) => (
                 <form onSubmit ={handleSubmit}>
                 <div className='add_room_name'>
@@ -37,6 +69,10 @@ class AddRooms extends Component{
                     name={'add_room_name'}
                     placeholder={'add room type'}
                     />
+                    <Error name='add_room_name' />
+                    {/* {
+                        this.add_room_name ? this.add_room_name : null
+                    } */}
                     </div>
 
                     <div className='add_room_form_group'>
@@ -46,33 +82,39 @@ class AddRooms extends Component{
                     name={'add_room_price'}
                     placeholder={'room price'}
                     />
+                    <Error name='add_room_price' />
+                    {
+                        this.add_room_price ? this.add_room_price : null
+                    }
                     </div>
 
                     <div className='add_room_form_group'>
-                    <label>Price(NRS.)</label>
+                    <label>Discount %</label>
                     <Inputfield
-                    type={'text'}
+                    type={'number'}
                     name={'add_room_price_discount'}
                     placeholder={'discount %'}
                     />
                     </div>
 
                     <div className='add_room_form_group'>
-                    <label>Price(NRS.)</label>
+                    <label>Room Count</label>
                     <Inputfield
                     type={'number'}
                     name={'add_room_count'}
                     placeholder={'room count'}
                     />
+                    <Error name='add_room_count' />
                     </div>
 
                     <div className='add_room_form_group'>
-                    <label>Price(NRS.)</label>
+                    <label>Maximum Capacity</label>
                     <Inputfield
                     type={'number'}
                     name={'add_max_capacity'}
                     placeholder={'maximum capacity'}
                     />
+                    <Error name='add_max_capacity' />
                     </div>
                 </div>
                 
@@ -93,7 +135,25 @@ class AddRooms extends Component{
                 }
                  <div className='add_room_description'>
                     <h4>Description</h4>
-                   <textarea type='textarea' cols='auto' rows='auto'>Room description</textarea>
+                   <textarea 
+                   type='textarea' 
+                   name='add_room_des'
+                   cols='auto' 
+                   rows='auto'>
+                   Room description
+                   </textarea>
+                   <Error name = 'add_room_des' />
+                    </div>
+                    <div className='add_room_description'>
+                    <h4>Rules</h4>
+                   <textarea 
+                   type='textarea'
+                   name='add_room_rules' 
+                   cols='auto' 
+                   rows='auto'>
+                   Rules
+                   </textarea>
+                   <Error name='add_room_rules' />
                     </div>
                     <div className='room_add_buttons'>
                         <button type='submit' className='room_cancel_btn'>cancel</button>
--- src/helpers/FormError.js
+++ src/helpers/FormError.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Field } from 'react-final-form';
+ 
+const err_style = {
+  color : 'red',
+  fontSize: '12px',
+  position: 'absolute'
 
+  
+}
 const Error = ({ name }) => (
     <Field name={name} subscription={{ error: true, touched: true }}>
       {({ meta: { error, touched } }) =>
-        error && touched ? <span>{error}</span> : null
+        error && touched ? <span style={err_style}>{error}</span> : null
       }
     </Field>
   )
