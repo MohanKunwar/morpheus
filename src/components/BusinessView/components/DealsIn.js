1d8a36e75adc85567c2727294adb89dc0278ad6b
Ganesh <ganeshkoirala999@gmail.com>
2019-04-29 15:54:36 +0545

    listing dealsin children in Deals In and not showing if no deals in

--- src/components/BusinessView/components/DealsIn.js
+++ src/components/BusinessView/components/DealsIn.js
@@ -7,29 +7,35 @@ class DealsIn extends Component {
     }
     componentWillMount() {
         Axios.instance.get(this.props.dealsIn).then(response => {
-            this.setState({ dealsIn: response.data.data });
+            this.setState({ dealsIn: response.data.data});
         })
     }
     render() {
-        let dealsInList = null;
+        let dealsInList= [];
         if (this.state.dealsIn && this.state.dealsIn.length > 0) {
-            dealsInList = this.state.dealsIn.map((item) => {
-                return (
-                    <div key={item.id} className='deals-in'>
-                        <div> {item.name} </div>
+            this.state.dealsIn.map((item) => {
+            if(item.children.length > 0) {
+           dealsInList.push(item.children.map((item_children, index) => {
+                  return <div key={index} className='deals-in'>
+                    <div> {item_children.name} </div>
                     </div>
-                )
+                })
+           )
+            }
             })
         }
         return (
             this.state.dealsIn ?
-                this.state.dealsIn.length > 0 ?
+            dealsInList.length > 0 ?
                     <div className='business_section'>
                         <div className="business_heading">
                             Deals In
+                            {console.log("wow", dealsInList.length)}
                         </div>
                         <div className='deals_in_section'>
-                            {dealsInList}
+                            {dealsInList.map((dealsInList) => {
+                                return dealsInList
+                            })}
                         </div>
                     </div>
                     : null
